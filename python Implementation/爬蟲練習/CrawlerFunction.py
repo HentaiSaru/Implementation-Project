bc9d5b7f0de55f68f053ddfbc3b049b5f50a2805
HentaiSaru <s3459897@gmail.com>
2023-03-31 09:29:18 +0800

    BiliBil 爬蟲完成

--- python Implementation/爬蟲練習/CrawlerFunction.py
+++ python Implementation/爬蟲練習/CrawlerFunction.py
@@ -1,13 +1,20 @@
-from tkinter import messagebox
+import random
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium import webdriver
+from urllib.parse import unquote
 from bs4 import BeautifulSoup
 import requests
-import chardet
+import logging
 import time
-import re
+import sys
 import os
 
-def RequestsUse(URL):
+# 巴哈爬蟲
+def RequestsGamer(URL):
     os.system("color 9f")
 
     if URL.find("page=") == -1:
@@ -69,17 +76,123 @@ def RequestsUse(URL):
 
         print("========== Page:{}結尾 ==========\n".format(page))
 
-def SeleniumUse():
+# BiliBil 爬蟲
+def RequestsBiliBili(Input):
+    print("\n開始搜尋...")
 
-    Url = "https://www.gamer.com.tw/"
+    options = Options()
+    options.add_argument('--headless') # 此行關閉操作窗口
+    # 以下為繞過驗證爬蟲的機制
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+    port = random.randint(1024, 65535)
+    options.add_argument(f'--remote-debugging-port={port}')
+    options.add_argument("--log-level=3") # 關閉日誌訊息
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # 開啟網頁
+    driver = webdriver.Chrome(options=options)
+    # 繞過檢測Js
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    # 到達首頁
+    driver.get("https://www.bilibili.com/")
+    # 找到輸入框
+    search = WebDriverWait(driver,30).until(EC.presence_of_element_located((By.XPATH, "//input[@class='nav-search-input']")))
+    # 點選
+    search.click()
+    # 輸入
+    search.send_keys(Input)
+    # 送出
+    search.send_keys(Keys.RETURN)
 
-    # 模擬 headers
-    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
-    opt = webdriver.ChromeOptions()
+    # 切換分頁
+    driver.switch_to.window(driver.window_handles[-1])
+    # 獲取新分頁的網址
+    Newurl = driver.current_url
 
-    # 載入 headers 資訊
-    opt.add_argument("--user-agent=%s" % UserAgent)
-    driver = webdriver.Chrome('./chromedriver', options=opt)
-    driver.get(Url)
+    """ selenium 的解析的爬取方式
+        # 開始爬取資料
+        Content = driver.page_source.encode('utf-8').strip()
+        html = BeautifulSoup(Content,'html.parser')
+        # 取得資料
+        finalPage = html.select('button.vui_button.vui_button--no-transition.vui_pagenation--btn.vui_pagenation--btn-num')[-1]
+        VideoTitle = html.find_all('h3')
+        VideoTime = html.select("span.bili-video-card__info--date")
+        VideoURL = html.select("div.bili-video-card__info--right")
 
-RequestsUse("https://forum.gamer.com.tw/B.php?page=24&bsn=30861")
+        time.sleep(3)
+        os.system("cls")
+
+        for i in range(len(VideoTitle)):
+            Tag = VideoURL[i].find("a")
+            href = Tag['href']
+
+            Vtime = VideoTime[i].text
+            Vtitle = VideoTitle[i].text
+
+            print("{}\n{}\n{}\n".format(Vtime,Vtitle,"https:"+href))
+    """
+    os.system("cls")
+    def UrlPattern(URL):
+        # 大坑 很重要!! 當網址含有中文,在複製時會被轉譯,這邊是將他轉回去
+        NewURL = unquote(URL)
+        # 用於判斷 BiliBil 影片網址的後綴,並將其去除
+        if NewURL.find("/?") != -1:
+            NewURL = NewURL.split("/?")[0]
+        return NewURL
+
+    # === 換回 requests 爬取方式 ===
+    header = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
+    }
+
+    # 取得新頁面的URL並轉換格式
+    URL = UrlPattern(Newurl)
+    Information = requests.get(URL, headers=header)
+    html = BeautifulSoup(Information.text, "html.parser")
+
+    # 取得最後一頁
+    finalPage = html.select('button.vui_button.vui_button--no-transition.vui_pagenation--btn.vui_pagenation--btn-num')[-1]
+
+    # 從第一頁開始 ~ finalPage
+    current_page = 1
+    while current_page <= int(finalPage.text):
+        if current_page == 1: # 如果是第一頁,用預設的
+            Information = requests.get(Newurl, headers=header)
+            html = BeautifulSoup(Information.text, "html.parser")
+            List = html.select("div.bili-video-card")
+        else: # 之後的頁數
+            # 滾動頁面
+            driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+            # 找到下一頁按鈕
+            Next = WebDriverWait(driver,0).until(EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'vui_pagenation--btn-side') and contains(text(), '下一页')]")))
+            time.sleep(0.5)
+            # 按下按鈕
+            Next.click()
+            # 獲取新的網址格式
+            Newurl = UrlPattern(driver.current_url)
+            Information = requests.get(Newurl, headers=header)
+            htmlNew = BeautifulSoup(driver.page_source, "html.parser") 
+            #driver.execute_script("return document.documentElement.outerHTML")JavaScript代碼在瀏覽器中執行並返回當前頁面的完整HTML源代碼
+            #driver.page_source 也是另一種返回當前頁面的完整HTML源代碼的方法
+            List = htmlNew.select("div.bili-video-card")
+            time.sleep(3)
+
+        # 開始爬取頁面
+        for i in List:
+            try:
+                VideoTitle = i.select("h3")[0].text.strip()
+                VideoTime = i.select("span.bili-video-card__info--date")[0].text.strip()
+                VideoURL = i.select("div.bili-video-card__info--right")[0].find("a")["href"]
+                print("{}\n{}\n{}\n".format(VideoTime,VideoTitle,"https:"+VideoURL))
+            except Exception as e:
+                print("debug:{}".format(e))
+                continue
+        print("========== Page:{}結尾 ==========\n".format(current_page))
+        current_page += 1 # 每爬完一頁就+1
+    driver.quit() # 關閉端口避免出錯 
+
+RequestsBiliBili(input("輸入你的查詢: "))
+#RequestsGamer("https://forum.gamer.com.tw/B.php?bsn=36390")
+input("\n運行完畢...")
