97699dc3012ae6c3217bc87b8901d9ba07673cd0
HentaiSaru <s3459897@gmail.com>
2023-04-22 23:46:34 +0800

    測試

--- python Implementation/遊戲操作腳本專案(未實施)/Automation/TensorConversion.py
+++ python Implementation/遊戲操作腳本專案(未實施)/Automation/TensorConversion.py
@@ -1,10 +1,8 @@
+import torchvision.transforms as transforms
+from PIL import Image
 import torch
 import cv2
 import os
-
-path = rf"{os.path.dirname(os.path.abspath(__file__))}\Image"
-file_names = os.listdir(path)
-
 
 # 檢測 torch 版本 , 如果安裝到 cpu 版本會顯示 None  , 重載(https://pytorch.org/get-started/locally/)
 #print(torch.version.cuda)
@@ -12,12 +10,52 @@ file_names = os.listdir(path)
 # 檢測是否能使用 cuda , 開發包(https://developer.nvidia.com/cuda-downloads?target_os=Windows&target_arch=x86_64)
 #print(torch.cuda.is_available())
 
-tensor_list = []
+#print(torch.cuda.get_device_name())
 
-# for file_name in file_names:
-#     img_path = os.path.join(path,file_name).split("\\")[-1]
-#     img = cv2.imread(img_path,cv2.IMREAD_GRAYSCALE)
-#     tensor_img = torch.from_numpy(img).permute(2, 0, 1).float() / 255.0
-#     tensor_list.append(tensor_img)
+"""
+用於機器學習的,圖像張量轉換
+"""
 
-# torch.save(tensor_list, r'\Image\Data.pt')
+preprocess = transforms.Compose([
+    transforms.RandomResizedCrop((224, 224), scale=(0.8, 1.0)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ColorJitter(brightness=(0, 1), contrast=(0, 1), saturation=(0, 1), hue=(-0.5, 0.5)),
+    transforms.ToTensor(),
+    transforms.Lambda(lambda x: x[:3]),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# 資料夾路徑
+path = rf"{os.path.dirname(os.path.abspath(__file__))}\Image"
+os.chdir(path)
+
+Picturename = ""
+for filename in os.listdir(path):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        template = []
+        # 讀取圖片
+        image_path = os.path.join(path, filename)
+        image = Image.open(image_path)
+        Picturename = filename.split(".")[0]
+        for _ in range(10):
+            # 預處理圖片
+            preprocessed_image = preprocess(image)
+
+            # 將預處理後的圖片轉換為張量，並加入張量列表中
+            template_tensor = torch.unsqueeze(preprocessed_image, 0)
+            template.append(template_tensor)
+
+        # 將張量列表轉換為模板張量，即將所有張量進行串接
+        template_tensor = torch.cat(template, dim=0)
+
+        # 儲存模板張量到檔案
+        torch.save(template_tensor, f'{Picturename}.pt')
+
+merge = []
+for filename in os.listdir(path):
+    if filename.endswith(".pt"):
+        merge.append(torch.load(filename))
+        os.remove(filename)
+
+combined_template = torch.cat(merge, dim=0)
+torch.save(combined_template, 'final.pt')
--- python Implementation/遊戲操作腳本專案(未實施)/Automation/項目實施測試.py
+++ python Implementation/遊戲操作腳本專案(未實施)/Automation/項目實施測試.py
@@ -1,5 +1,8 @@
+import torchvision.transforms as transforms
+import concurrent.futures
 import numpy as np
 import pyautogui
+import threading
 import torch
 import time
 import cv2
@@ -29,15 +32,18 @@ class ScreenCapture:
         # 窗口大小
         cv2.resizeWindow("Screen",win_size_w,win_size_h)
         cv2.setWindowProperty("Screen", cv2.WND_PROP_TOPMOST, 1)
+        # 設置線程數
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
 
         with mss.mss() as sct:
-            
             while True:
                 time.sleep(self.refresh)
                 screen = sct.grab(capture)
                 img = np.array(screen)
+
                 # 找到匹配圖
-                Seek.Find(img)
+                #Seek.Find(img)
+                executor.submit(Seek.Find, img)
 
                 # 重新縮放窗口圖片大小
                 img = cv2.resize(img, (win_size_h, win_size_w))
@@ -58,31 +64,47 @@ class ScreenCapture:
                 screen = sct.grab(capture)
 
 class Match:
-    def __init__(self,img,threshold):
+    def __init__(self,img,threshold,device=torch.device('cuda')):
         self.template = []
         self.template_names = []
         self.match = threshold
-        for templates in img:
-            self.template.append(cv2.imread(templates,cv2.IMREAD_GRAYSCALE))
-            self.template_names.append(templates)
+        self.device = device
 
+        for tensor in torch.load(img):
+            print(tensor)
+
+        # for img_path in img:
+        #     # 讀取圖片並轉換為灰度圖像
+        #     img = cv2.imread(img_path)
+        #     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        #     # 將轉換後的圖像張量加入模板列表
+        #     tensor_gray = torch.from_numpy(img_gray).unsqueeze(0).unsqueeze(0).to(self.device)
+        #     self.template.append(tensor_gray)
+
+        #     # 保存模板名稱
+        #     self.template_names.append(img_path)
+        # # 將模板轉移到GPU上
+        # self.template = [template.to(self.device) for template in self.template]
+        
     def Find(self,photo):
+        # 灰階轉換
+        img_gray = cv2.cvtColor(photo, cv2.COLOR_BGR2GRAY)
+        # # 將photo轉換為PyTorch張量，並移動到GPU
+        # img_tensor = torch.from_numpy(img_gray).unsqueeze(0).unsqueeze(0).float().to(self.device)
 
-        # 將掃描的螢幕轉成灰階
-        img_gray = cv2.cvtColor(photo,cv2.COLOR_BGR2GRAY)
+        # for index, template in enumerate(self.template):
+        #     # 使用conv2d函數進行匹配
+        #     res = torch.nn.functional.conv2d(img_tensor, template).squeeze()
 
-        for index , template in enumerate(self.template):
+        #     # 匹配狀態 (轉換回CPU處理,並保存到numpy數組)
+        #     loc = np.where(res.cpu().numpy() >= self.match)
 
-            # 將畫面與模板進行匹配
-            res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-            # 匹配狀態
-            loc = np.where(res >= self.match)
-
-            # 匹配成功
-            if len(loc[0]) > 0:
-                self.template_names[index]
-                #Action.draw(loc,template,photo)
-                #Action.action(loc,self.template)
+        #     # 匹配成功
+        #     if len(loc[0]) > 0:
+        #         self.template_names[index]
+        #         #Action.draw(loc,template,photo)
+        #         #Action.action(loc,self.template)
 
 class Operate:
     def __init__(self):
@@ -111,13 +133,16 @@ def get_screen_size():
     screen_size = pyautogui.size()
     return screen_size[0] , screen_size[1]
 
-Screen = ScreenCapture(144)
-Script = ["Image/test.png","Image/test2.png","Image/test3.png"]
-Seek = Match(Script,0.9)
-Action = Operate()
+# Screen = ScreenCapture(144)
+# Script = ["Image/final.pt"]
+# Seek = Match(Script,0.9)
+# Action = Operate()
 
-w , h = get_screen_size()
-capture = {"top": 0, "left": 0, "width": w, "height": h}
+# w , h = get_screen_size()
+# capture = {"top": 0, "left": 0, "width": w, "height": h}
 
-Screen.debug(capture)
+#Screen.debug(capture)
 #Screen.NoWindow(capture)
+
+tensor =torch.load("Image/final.pt")
+print(tensor)
