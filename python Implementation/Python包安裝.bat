c18c79919504c362d93b15a8326d68566b83f2fb
HentaiSaru <s3459897@gmail.com>
2023-04-24 08:53:50 +0800

    更新

--- python Implementation/Python包安裝.bat
+++ python Implementation/Python包安裝.bat
@@ -100,6 +100,22 @@ pip install opencv-python
 :: https://github.com/opencv/opencv/tree/4.7.0
 :: https://github.com/opencv/opencv_contrib/tree/4.7.0
 
+:: 編譯設置
+:: WITH_CUDA -> 開
+:: OPENCV_DNN_CUDA -> 開
+:: ENABLE_FAST_MATH -> 開
+:: PYTHON -> 看到能開的都開(除了有test的)
+:: OPENCV_EXTRA_MODULES_PATH -> 指定opencv_contrib的modules
+:: BUILD_opencv_world -> 開
+:: OPENCV_ENABLE_NONFREE -> 開
+:: CUDA_ARCH_BIN -> 根據顯卡算力設置
+:: WITH_CUDNN -> 開
+:: CUDA_FAST_MATH -> 開
+:: test -> 可以都關掉
+:: java -> 可以都關掉
+:: OPENCV_GENERATE_SETUPVARS -> 關
+:: 最後開啟 OpenCV.sln -> 用 vs 並且編譯 opencv_python3
+
 :: 處理鍵盤操作
 pip install keyboard
 :: 控制和監控輸入設備
--- python Implementation/遊戲操作腳本專案(未實施)/Automation/GPU測試.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-if cv2.cuda.getCudaEnabledDeviceCount() > 0:
-    print("可以使用GPU加速")
-else:
-    print("無法使用GPU加速")
