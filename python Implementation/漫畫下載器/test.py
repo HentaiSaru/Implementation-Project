8ca83ef4bb4179fe6dd5459b441e4df04eee7929
HentaiSaru <s3459897@gmail.com>
2023-04-26 11:16:50 +0800

    測試

--- python Implementation/漫畫下載器/test.py
+++ python Implementation/漫畫下載器/test.py
@@ -1,4 +1,9 @@
+import random
+import undetected_chromedriver as uc
+import multiprocessing
+import threading
 import requests
+import time
 import os
 dir = os.path.abspath("R:/") # 可更改預設路徑
 os.chdir(dir)
@@ -6,6 +11,18 @@ os.chdir(dir)
 header = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
 }
+chrome_options = ""
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-popup-blocking")
+chrome_options.add_argument("--profile-directory=Default")
+chrome_options.add_argument("--ignore-certificate-errors")
+chrome_options.add_argument("--disable-plugins-discovery")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument('--no-first-run')
+chrome_options.add_argument('--no-service-autorun')
+chrome_options.add_argument('--no-default-browser-check')
+chrome_options.add_argument('--password-store=basic')
+chrome_options.add_argument('--no-sandbox')
 
 def Download(Path,SaveName,Image_URL,headers):
 
@@ -14,4 +31,13 @@ def Download(Path,SaveName,Image_URL,headers):
             with open(SaveName,"wb") as f:
                 f.write(ImageData.content)
 
-Download(dir,"測試.jpg","",header)
+def test():
+    Settings = uc.ChromeOptions()
+    Settings.add_argument(f"--remote-debugging-port={random.randint(1024,65535)}")
+    browser = uc.Chrome(Settings)
+    browser.get("https://www.google.com.tw/")
+    time.sleep(10)
+
+
+for _ in range(5):
+    threading.Thread(target=test).start()
