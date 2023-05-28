2957f8feaa2af35d18445f4b44c39d19fae82041
TenshinoOtoKafu <s3459897@gmail.com>
2023-05-28 15:16:12 +0800

    剪貼簿擷取

---
+++ python Implementation/操作擷取/ScrapbookCapture.py
@@ -0,0 +1,38 @@
+import threading
+import pyperclip
+import time
+import os
+
+class Scrapbook:
+    def __init__(self):
+        self.fetch_cache = None
+        self.delay = None
+        self.clear = None
+        self.result = []
+
+    def __call__(self,delay:int=0.1 , clear:bool=False):
+
+        self.delay = delay
+        self.clear = clear
+
+        threading.Thread(target=self.capture).start()
+
+    def capture(self):
+        pyperclip.copy('')
+        
+        while True:
+            time.sleep(self.delay)
+            clipboard = pyperclip.paste()
+
+            if clipboard != self.fetch_cache:
+                self.fetch_cache = clipboard
+                self.result.append(clipboard)
+
+                if self.clear:
+                    os.system("cls")
+
+                print(clipboard)
+
+    def get_result(self):
+        if len(self.result) != 0:
+            return self.result
