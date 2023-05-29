ccd877e15bacac8e539696fd2e98828f6f6c5089
TenshinoOtoKafu <s3459897@gmail.com>
2023-05-29 13:56:10 +0800

    新功能開發

--- Command Prompt/Check.py
+++ Command Prompt/Check.py
@@ -1,19 +1,35 @@
+import packaging.version as Ver
 import requests
+import os
 
 class Check_for_updates:
     def __init__(self):
+        # 倉庫網址
         self.url = "https://raw.githubusercontent.com/TenshinoOtoKafu/Implementation-Project/Main/Command%20Prompt/System-Cleaning.bat"
-        self.text = []
+        self.Web_Version = None
+        self.Local_Version = None
 
     def Get_web(self):
         reques = requests.get(self.url)
         text = reques.text.split('\n')
+        self.Web_Version = text[0].split(" ")[3]
 
-        for line in text:
-            self.text.append(line)
+    def Get_local(self):
+        # 確保找當前路徑下
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-        print(self.text)
+        with open("System-Cleaning.bat","r",encoding="utf-8") as f:
+            self.Local_Version = f.readline().split(" ")[3]
+
+    def Update_Comparison(self):
+
+        if float(self.Web_Version) > float(self.Local_Version):
+            print("需更新")
+        else:
+            print("無需更新")
 
 if __name__ == "__main__":
     check = Check_for_updates()
     check.Get_web()
+    check.Get_local()
+    check.Update_Comparison()
