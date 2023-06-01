be1a99e7489b803068fa752051ee4a0ed5169d12
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-01 14:36:42 +0800

    添加網路檢測

--- Command Prompt/進階版-更新檢測.py
+++ Command Prompt/進階版-更新檢測.py
@@ -1,10 +1,12 @@
 from packaging.version import Version
+from tkinter import messagebox
 from tqdm import tqdm
 import subprocess
 import requests
+import socket
 import os
 
-""" Versions 1.0.0
+""" Versions 1.0.1
 
 - 進階版檢測
 
@@ -20,7 +22,14 @@ class Read_web_page:
         
         self.url = "https://raw.githubusercontent.com/TenshinoOtoKafu/Implementation-Project/Main/Command%20Prompt/System-Cleaning.bat"
         self.content = None
-        self.process = None
+
+    @staticmethod
+    def check_internet_connection():
+        try:
+            socket.create_connection(("www.github.com", 80))
+            return True
+        except (socket.error, OSError):
+            return False
 
     def Data_request(self):
         reques = requests.get(self.url)
@@ -34,26 +43,35 @@ class Read_web_page:
                 f.write(content + "\n")
 
     def Clean_run(self):
-        if os.path.exists(self.Location) != True:
-            self.Write_cache()
+        if self.check_internet_connection():
+            if os.path.exists(self.Location) != True:
+                self.Write_cache()
+            else:
+                self.Data_request()
+
+                Web_Version = self.content[0].split(" ")[3]
+
+                with open(self.Location ,"r",encoding="utf-8") as f:
+                    Local_Version = f.readline().split(" ")[3]
+
+                if Version(Web_Version) > Version(Local_Version):
+                    pbar = tqdm(total=len(self.content),ncols=80,desc="更新 ",bar_format="{l_bar}{bar}")
+
+                    with open(self.Location,"w",encoding="utf-8") as f:
+                        for text in self.content:
+                            f.write(text + "\n")
+                            pbar.update(1)
+                        pbar.clear()
+
+            subprocess.call(self.Location, shell=True)
+
         else:
-            self.Data_request()
+            messagebox.showerror("連線失敗","請確認網路連線\n嘗試無驗證運行",parent=None)
 
-            Web_Version = self.content[0].split(" ")[3]
-
-            with open(self.Location ,"r",encoding="utf-8") as f:
-                Local_Version = f.readline().split(" ")[3]
-
-            if Version(Web_Version) > Version(Local_Version):
-                pbar = tqdm(total=len(self.content),ncols=80,desc="更新 ",bar_format="{l_bar}{bar}")
-
-                with open(self.Location,"w",encoding="utf-8") as f:
-                    for text in self.content:
-                        f.write(text + "\n")
-                        pbar.update(1)
-                    pbar.clear()
-
-        subprocess.call(self.Location, shell=True)
+            if os.path.exists(self.Location):
+                subprocess.call(self.Location, shell=True)
+            else:
+                messagebox.showerror("嘗試失敗","請重新連接網路後運行",parent=None)
 
 if __name__ == "__main__":
    read = Read_web_page()
