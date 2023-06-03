f43af6ab76688815de8afc4eac0f5fe1ab6dacfb
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-03 19:19:07 +0800

    新調用

--- python Implementation/操作擷取/Main.py
+++ python Implementation/操作擷取/Main.py
@@ -1,10 +1,15 @@
-import ScrapbookCapture as Sb
+import ScrapbookCapture as sb
+import KeyboardCapture as kb
 import MouseCapture as ms
 
 # 剪貼簿擷取調用
-# SC = Sb.Scrapbook()
+# SC = sb.Scrapbook()
 # SC()
 
 # 滑鼠擷取調用
-mouse = ms.Mouse()
-mouse()
+# mouse = ms.Mouse()
+# mouse()
+
+# 鍵盤擷取調用
+key = kb.Keyboard()
+key()
