2a6a9d0a9c6e629cbdc02f07f8e8d44d4c12b8bf
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-03 17:50:43 +0800

    添加功能

--- python Implementation/操作擷取/MouseCapture.py
+++ python Implementation/操作擷取/MouseCapture.py
@@ -4,15 +4,21 @@ import time
 class capture():
     def __init__(self):
         self.Button = None
+        self.press_time = None
+        self.letgo_time = None
 
     def click(self, x, y, button, pressed):
-        if pressed:
-            left = "左鍵"
-            right = "右鍵"
-            middle = "中鍵"
+        left = "左鍵"
+        right = "右鍵"
+        middle = "中鍵"
 
+        if pressed:
+            self.press_time = time.time()
             self.Button = str(button).split("Button.")[1]
             print(f"按下了：{eval(self.Button)}")
+        else:
+            self.letgo_time = time.time()
+            print(f"按放開了：{eval(self.Button)} , 持續時間 : {self.letgo_time-self.press_time}")
 
     def move(self, x, y):
         print(f"X座標:{x} , Y座標:{y}")
