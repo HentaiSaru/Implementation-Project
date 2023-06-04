fc2460a15c75f5493bbf880af3646429c73c159d
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-04 21:15:37 +0800

    方法改進

--- python Implementation/測試項目/(Experiment)Animation_Wiki/Search_Settings.py
+++ python Implementation/測試項目/(Experiment)Animation_Wiki/Search_Settings.py
@@ -1,5 +1,7 @@
+from bs4 import BeautifulSoup
+from lxml import etree
 import requests
-# 數據請求的設置
+
 class default:
     def __init__(self):
         self.headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
@@ -8,4 +10,10 @@ class default:
     def data(self,url):
         return self.request.get(url,headers=self.headers)
     
-R_get = default()
+    def get_tree(self,url):
+        return etree.fromstring(self.data(url).content, etree.HTMLParser())
+
+    def get_bfs(self,url):
+        return BeautifulSoup(self.data(url).text,"html.parser")
+    
+ask_s = default()
