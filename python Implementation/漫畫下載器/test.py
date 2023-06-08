04b20a2a8a6e92720ccbca50a49a16b023103954
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-08 20:53:12 +0800

    功能開發測試

--- python Implementation/漫畫下載器/test.py
+++ python Implementation/漫畫下載器/test.py
@@ -1,26 +1,52 @@
+import undetected_chromedriver as uc
 import multiprocessing
 import pyperclip
 import threading
 import requests
 import keyboard
+import random
 import time
 import re
 import os
 dir = os.path.abspath("R:/") # 可更改預設路徑
 os.chdir(dir)
 
-header = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-}
-# chrome_options = ""
-# chrome_options.add_argument("--disable-extensions")
-# chrome_options.add_argument("--disable-popup-blocking")
-# chrome_options.add_argument("--profile-directory=Default")
-# chrome_options.add_argument("--ignore-certificate-errors")
-# chrome_options.add_argument("--disable-plugins-discovery")
-# chrome_options.add_argument("--incognito")
-# chrome_options.add_argument('--no-first-run')
-# chrome_options.add_argument('--no-service-autorun')
-# chrome_options.add_argument('--no-default-browser-check')
-# chrome_options.add_argument('--password-store=basic')
-# chrome_options.add_argument('--no-sandbox')
+url = "https://www.google.com.tw/"
+
+def settings():
+    Settings = uc.ChromeOptions()
+    Settings.add_argument("--incognito")
+    Settings.add_argument('--no-sandbox')
+    Settings.add_argument('--log-level=3')
+    Settings.add_argument('--no-first-run')
+    Settings.add_argument('--disable-infobars')
+    Settings.add_argument("--disable-extensions")
+    Settings.add_argument('--no-service-autorun')
+    Settings.add_argument("--disable-file-system")
+    Settings.add_argument("--disable-geolocation")
+    Settings.add_argument("--disable-web-security")
+    Settings.add_argument('--password-store=basic')
+    Settings.add_argument('--disable-notifications')
+    Settings.add_argument("--disable-popup-blocking") 
+    Settings.add_argument('--no-default-browser-check')
+    Settings.add_argument("--profile-directory=Default")
+    Settings.add_argument("--ignore-certificate-errors")
+    Settings.add_argument("--disable-plugins-discovery")
+    Settings.add_argument('--remote-debugging-address=0.0.0.0')
+    Settings.add_argument('--disable-blink-features=AutomationControlled')
+    Settings.add_argument(f"--remote-debugging-port={random.randint(1024,65535)}")
+    return Settings
+
+def browser():
+    return uc.Chrome(
+        options=settings(),
+    )
+
+def main():
+    driver = browser()
+    driver.get(url)
+    time.sleep(3)
+    driver.close()
+
+for _ in range(5):
+    threading.Thread(target=main).start()
