50bf6dc509a57907075d0384eaa06cc3e1692a8f
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-08 20:53:29 +0800

    修復 版本更新

--- python Implementation/漫畫下載器/NHentai漫畫下載.py
+++ python Implementation/漫畫下載器/NHentai漫畫下載.py
@@ -22,7 +22,7 @@ os.chdir(dir)
     * 使用說明
     
     爬蟲適用網站 : https://nhentai.net/
-    !! 下載速度慢是正常的 , 他是模擬人類操作 , 一頁一頁去下載圖片
+    !! 下載速度慢是正常的 , 他是模擬人類操作 , 一頁一頁去下載圖片 (主要還是網路速度,和網頁加載速度影響)
 
     * 開發說明
 
@@ -36,11 +36,13 @@ os.chdir(dir)
 
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    Versions 1.0.0
+    Versions 1.0.1
 
-    [+] 單本漫畫下載
-    [+] 搜尋頁面下載
-    [+] 自動擷取複製
+    [+] 修復網址驗證處理
+    [+] 新增下載類型的支援
+
+    [-] 刪除多線程測試
+    [-] 刪除使用者數據位置
 
     後續開發預想
 
@@ -62,19 +64,36 @@ def reset():
     os.system("pip install --upgrade setuptools >nul 2>&1")
     os.system("pip install --upgrade wheel >nul 2>&1")
     print("重置完成...")
+
 # 取得自動化操作設置
 class Automation:
     def __init__(self,head):
         self.configuration = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])),"drive")
-        self.drivepath = os.path.join(self.configuration,'112','chromedriver.exe')
-        self.UserData = os.path.join(dir,"Data")
+        self.drivepath = os.path.join(self.configuration,'114','chromedriver.exe')
         
         # 選項設置
         self.Settings = uc.ChromeOptions()
         if head:self.Settings.add_argument("--headless")
-        self.Settings.add_argument(f"--remote-debugging-port={random.randint(1024,65535)}")
+        self.Settings.add_argument("--incognito")
+        self.Settings.add_argument('--no-sandbox')
+        self.Settings.add_argument('--log-level=3')
+        self.Settings.add_argument('--no-first-run')
+        self.Settings.add_argument('--disable-infobars')
         self.Settings.add_argument("--disable-extensions")
-        self.Settings.add_argument('--no-first-run --no-service-autorun --password-store=basic')
+        self.Settings.add_argument('--no-service-autorun')
+        self.Settings.add_argument("--disable-file-system")
+        self.Settings.add_argument("--disable-geolocation")
+        self.Settings.add_argument("--disable-web-security")
+        self.Settings.add_argument('--password-store=basic')
+        self.Settings.add_argument('--disable-notifications')
+        self.Settings.add_argument("--disable-popup-blocking") 
+        self.Settings.add_argument('--no-default-browser-check')
+        self.Settings.add_argument("--profile-directory=Default")
+        self.Settings.add_argument("--ignore-certificate-errors")
+        self.Settings.add_argument("--disable-plugins-discovery")
+        self.Settings.add_argument('--remote-debugging-address=0.0.0.0')
+        self.Settings.add_argument('--disable-blink-features=AutomationControlled')
+        self.Settings.add_argument(f"--remote-debugging-port={random.randint(1024,65535)}")
 
     # 創建驅動路徑(這樣後續開啟會比較快)
     def driver_creation(self):
@@ -88,74 +107,71 @@ class Automation:
     def browser(self):
         try:
             return uc.Chrome(
-                version_main=112,
+                version_main=114,
                 options=self.Settings,
-                driver_executable_path=self.drivepath,
-                user_data_dir=self.UserData
+                driver_executable_path=self.drivepath
             )
         except:
+            print("驅動下載中...")
             self.driver_creation()
             return uc.Chrome(
-                version_main=112,
+                version_main=114,
                 options=self.Settings,
                 driver_executable_path=self.drivepath,
-                user_data_dir=self.UserData
             )
 # 漫畫主頁處理
 class Verify:
     def __init__(self,enter,pages,head):
         self.search = r"https://nhentai\.net/.*"
-        self.mangapage = r"https://nhentai\.net/g/\d+"
+        self.manga = r"https://nhentai\.net/g/\d+"
+
+        self.search_page = []
+        self.manga_page = []
+        self.work_list = []
 
         self.pages = pages
         self.head = head
-
-        self.search_processing = []
-        self.manga_processing = []
-        self.integration_request = []
-
+        self.URL_Classification(enter)
     
-    """ 等待修復 (累了先睡覺)
+    def URL_Classification(self,enter):
+
+        def Page_type(url):
+            if re.match(self.manga,url):
+                self.manga_page.append(url)
+            elif re.match(self.search,url):
+                self.search_page.append(url)
+            else:
+                print(f"錯誤格式的連結{url}")
 
         if isinstance(enter,list):
-            for data in enter:
-               start = threading.Thread(target=self.TypeDetection,args=(data,))
-               start.start()
-               start.join()
+            for url in enter:
+                Page_type(url)
         else:
-            start = threading.Thread(target=self.TypeDetection,args=(data,))
-            start.start()
-            start.join()
+            Page_type(enter)
 
-        start = threading.Thread(target=self.MangaPage)
-        start.start()
-        start.join()
-        start = threading.Thread(target=self.SearchPage)
-        start.start()
-        start.join()
-        self.run(self.integration_request)
-
-    def TypeDetection(self,enter):
-        
-        if re.match(self.mangapage,enter):
-            self.manga_processing.append(enter)
-        elif re.match(self.search,enter):
-            self.search_processing.append(enter)
-        else:print(f"錯誤格式的連結{enter}")
+        # 處理完成後呼叫後續
+        self.MangaPage()
+        self.SearchPage()
+        # 處理完成呼叫下載
+        self.run(self.work_list)
 
     def MangaPage(self):
-        if len(self.manga_processing) != 0:
-            for data in self.manga_processing:
-                self.integration_request.append(data)
+        if len(self.manga_page) != 0:
+            for data in self.manga_page:
+                self.work_list.append(data)
 
     def SearchPage(self):
-        if len(self.search_processing) != 0:
-            for enter in self.search_processing:
+        if len(self.search_page) != 0:
+            print("批量下載操作處理,請稍後...")
+            for data in self.search_page:
                 Auto = Automation(self.head)
                 browser = Auto.browser()
 
-                if enter.find("?page") != -1:url = f"{enter.split('?page=')[0]}?page=1"
-                
+                if data.find("?page") != -1:
+                    url = f"{data.split('?page=')[0]}?page=1"
+                else:
+                    url = data
+
                 # 首次開啟
                 browser.get(url)
 
@@ -167,7 +183,7 @@ class Verify:
                 if self.pages > int(page):self.pages = int(page)
 
                 for a in html.xpath("//a[@class='cover']"):
-                    self.integration_request.append(rf"https://nhentai.net{a.get('href')}")
+                    self.work_list.append(rf"https://nhentai.net{a.get('href')}")
 
                 if self.pages > 1:
                     pbar = tqdm(total=self.pages,desc="正在處理批量網址")
@@ -176,16 +192,18 @@ class Verify:
                     WebDriverWait(browser,30).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='container index-container']")))
                     html = etree.fromstring(browser.page_source,etree.HTMLParser())
                     for a in html.xpath("//a[@class='cover']"):
-                        self.integration_request.append(rf"https://nhentai.net{a.get('href')}")
+                        self.work_list.append(rf"https://nhentai.net{a.get('href')}")
                     if page == self.pages:pbar.update(2)
                     else:pbar.update(1)
+                
+                # 沒有設置使用者數據位置使用 close() , 有設置用 quit()
+                browser.close()
                 pbar.close()
-        else:pass
             
     # 開始請求
     def run(self,url):
-        print("開始操作請稍後...\n")
-        for deal in url:download.Data_Request(deal) """
+        print("開始下載操作...\n")
+        download.Data_Request(url)
 
 class ComicsHomePage:
     def __init__(self):
@@ -193,60 +211,62 @@ class ComicsHomePage:
         self.title = ""     # 漫畫名稱
         self.labelbox = {}  # 保存標籤數據
         self.Home = None    # 主頁Html存放
-        self.pages = 1      # 下載頁數
+        self.pages = 10      # 下載頁數
         self.SaveNameFormat = 1
         self.headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
         self.UserData = os.path.join(dir,"Data")
 
-    def enter(self,Url: str,pages: int=None,head: bool=True):
+    def enter(self, Url, pages: int=None, head: bool=True):
         if pages != None:self.pages = pages
         if head == False:self.head = head
 
         os.system(f"RD /s /q {self.UserData} >nul 2>&1")
         Verify(Url,self.pages,self.head)
     
-    def Data_Request(self,Url):
+    def Data_Request(self,box):
         Auto = Automation(self.head)
         browser = Auto.browser()
 
-        # 開啟漫畫主頁
-        browser.get(Url)
+        for Url in box:
+            # 開啟漫畫主頁
+            browser.get(Url)
 
-        try: # 測試點選機器人認證
-            human = WebDriverWait(browser,1).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='checkbox']")))
-            human.click()
-        except:pass
-        
-        # 測試延遲處理
-        WebDriverWait(browser,30).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='thumbs']")))
+            try: # 測試點選機器人認證
+                human = WebDriverWait(browser,1).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='checkbox']")))
+                human.click()
+            except:pass
 
-        # 取得主頁代碼,並呼叫處理
-        self.Home = etree.fromstring(browser.page_source,etree.HTMLParser())
-        self.Data_Processing()
+            # 延遲處理
+            WebDriverWait(browser,60).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='thumbs']")))
 
-        # 處理完成後,獲取總共頁數,和創建資料夾
-        Pages = self.labelbox['Pages'][0]
-        self.Folder_Creation(self.title)
+            # 取得主頁代碼,並呼叫處理
+            self.Home = etree.fromstring(browser.page_source,etree.HTMLParser())
+            self.Data_Processing()
 
-        print(f"{self.title}\n開始下載 ==>")
-        # 創建進度條
-        pbar = tqdm(total=int(Pages))
-        
-        for page in range(1,int(Pages)+1):
-            browser.get(f"{Url}/{page}/")
-            ImgUrl = etree.fromstring(browser.page_source,etree.HTMLParser()).xpath("//section[@id='image-container']/a/img")[0].get('src')
-            
-            if int(Pages) >= 100:SaveName = f"{self.SaveNameFormat:03d}.{ImgUrl.split('.')[-1]}"
-            else:SaveName = f"{self.SaveNameFormat:02d}.{ImgUrl.split('.')[-1]}"
+            # 處理完成後,獲取總共頁數,和創建資料夾
+            Pages = self.labelbox['Pages'][0]
+            self.Folder_Creation(self.title)
 
-            threading.Thread(target=self.Download,args=(os.path.join(dir,self.title),SaveName,ImgUrl,self.headers)).start()
-            
-            self.SaveNameFormat += 1
-            pbar.update(1)
+            print(f"{self.title}\n開始下載 ==>")
+            # 創建進度條
+            pbar = tqdm(total=int(Pages))
 
-        self.SaveNameFormat = 1
-        pbar.close()
-        browser.quit()
+            for page in range(1,int(Pages)+1):
+                browser.get(f"{Url}/{page}/")
+                ImgUrl = etree.fromstring(browser.page_source,etree.HTMLParser()).xpath("//section[@id='image-container']/a/img")[0].get('src')
+
+                if int(Pages) >= 100:SaveName = f"{self.SaveNameFormat:03d}.{ImgUrl.split('.')[-1]}"
+                else:SaveName = f"{self.SaveNameFormat:02d}.{ImgUrl.split('.')[-1]}"
+
+                threading.Thread(target=self.Download,args=(os.path.join(dir,self.title),SaveName,ImgUrl,self.headers)).start()
+
+                self.SaveNameFormat += 1
+                pbar.update(1)
+
+            self.SaveNameFormat = 1
+            pbar.close()
+
+        browser.close()
 
     # 處理主頁數據
     def Data_Processing(self):
@@ -314,30 +334,30 @@ class AutomaticCapture:
 
 if __name__ == "__main__":
 
-    # 當無法正常啟用自動化窗口時 , 就啟用該方法 !!Google會被關掉
+    #* 當無法正常啟用自動化窗口時 , 就啟用該方法 !!Google會被關掉
     # reset()
 
-    # list 傳遞目前只適用於漫畫連結 , 搜尋連結不適用 
+    #* list 傳遞
     Batch_Box = []
 
 ###########################################################
 
     #? 客製化下載方法
 
-    # url , 頁數 , 隱藏窗口 (url可輸入,單本漫畫/搜尋連結/Batch_Box的list)
-    # download.enter("")
+    #* url(必要) , 頁數(非必要/搜尋頁面預設10) , 隱藏窗口(非必要/True,False/預設是True) (url[單本漫畫/搜尋連結/Batch_Box的list])
+    download.enter("#")
 
 ###########################################################
 
-    #? 自動擷取剪貼簿(只適用單本漫畫連結)
+    #? 自動擷取剪貼簿
 
-    print("自動擷取下載(如要使用搜尋頁面,使用客製化方法下載)\n複製網址完畢後 , 按下 Alt+S 開始下載")
+    # print("自動擷取下載(如要使用搜尋頁面,使用客製化方法下載)\n複製網址完畢後 , 按下 Alt+S 開始下載")
+    # capture = AutomaticCapture()
 
-    capture = AutomaticCapture()
     # 監聽剪貼版線程
-    threading.Thread(target=capture.Read_clipboard).start()
+    # threading.Thread(target=capture.Read_clipboard).start()
 
     # 監聽鍵盤觸發下載(設置為守護線程,當主線程結束,守護線程自動終止)
-    command = threading.Thread(target=capture.Download_command)
-    command.daemon = True
-    command.start()
+    # command = threading.Thread(target=capture.Download_command)
+    # command.daemon = True
+    # command.start()
