59ee69741f9d0a7eda84e02a6523470d3c983370
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-14 01:18:59 +0800

    線程測試

--- python Implementation/漫畫下載器/test.py
+++ python Implementation/漫畫下載器/test.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor , ProcessPoolExecutor
 import undetected_chromedriver as uc
 import multiprocessing
 import pyperclip
@@ -48,5 +49,32 @@ def main():
     time.sleep(3)
     driver.close()
 
-for _ in range(5):
-    threading.Thread(target=main).start()
+
+class Test:
+    def __init__(self):
+        self.manager = multiprocessing.Manager()
+        self.Queue = self.manager.Queue()
+        self.CpuCore = multiprocessing.cpu_count()
+        self.comic_link_box = ["a", 'b', 'c', "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"]
+
+    def ran1(self):
+        with ProcessPoolExecutor(max_workers=self.CpuCore, initializer=self.initialize_queue, initargs=(self.Queue,)) as executor:
+            for url in self.comic_link_box:
+                executor.submit(self.ran2, url)
+
+    @staticmethod
+    def initialize_queue(queue):
+        Test.Queue = queue
+
+    @staticmethod
+    def ran2(url):
+        t = Test()
+        t.Queue.put(url)
+        
+        # 从队列中获取数据
+        data = t.Queue.get()
+        print(data)
+
+if __name__ == '__main__':
+    t = Test()
+    t.ran1()
