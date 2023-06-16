626cc0fefa438668fcd818d69a2f4b6dfdc9e879
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-17 01:48:39 +0800

    使用修正

--- python Implementation/操作擷取/Main.py
+++ python Implementation/操作擷取/Main.py
@@ -3,13 +3,13 @@ import KeyboardCapture as kb
 import MouseCapture as ms
 
 # 剪貼簿擷取調用
-# SC = sb.Scrapbook()
-# SC()
+SC = sb.Scrapbook()
+SC()
 
 # 滑鼠擷取調用
 # mouse = ms.Mouse()
 # mouse()
 
 # 鍵盤擷取調用
-key = kb.Keyboard()
-key()
+# key = kb.Keyboard()
+# key()
