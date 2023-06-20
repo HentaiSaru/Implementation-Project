afda7f3f9b4debdbfeb64d7095a43e1600d06ed7
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-20 11:14:04 +0800

    測試功能

--- python Implementation/測試項目/開發測試.py
+++ python Implementation/測試項目/開發測試.py
@@ -3,15 +3,15 @@ import requests
 
 session = requests.Session()
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
-url = "https://www.youtube.com/"
 
+url = "https://files.catbox.moe/dz64k5.mp4"
 filterdomains=["google.com","bing.com","youtube.com","facebook.com","line.me","sharepoint.com","taobao.com","shopee.tw"]
 
-data = session.get(url, headers=headers)
+#data = session.get(url, headers=headers)
+data = session.head(url , headers=headers)
 
 # if data.url == url:
     # print("錯誤的")
 
-
-test = normalize('NFKC', url).encode('ascii', 'ignore').decode('ascii').strip()
-print(test)
+print(data.status_code)
+print(data.url)
