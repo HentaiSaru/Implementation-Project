92239905531184641f149b49e456cc52d9cd853d
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-28 23:59:09 +0800

    腳本輸入輸出

---
+++ python Implementation/OperationRecording/Package/Operation_Logic/Record_Read.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+#Todo - 腳本保存 與 腳本讀取 -
+
+class InputOutput:
+    def __init__(self):
+        self.script_path = "{}\\Script".format(os.path.dirname(os.path.abspath(__file__)).rsplit("\\",2)[0])
+        self.script_data = {}
+
+        # 判斷腳本目錄是否存在 , 不存在創建
+        if not os.path.exists(self.script_path):
+            os.mkdir(self.script_path)
+
+    def Save_Script(self, save_name: str, save_data: dict):
+        save_path = os.path.join(self.script_path,f"{save_name}.json")
+
+        with open(save_path , "w") as file:
+            file.write(json.dumps(save_data))
+
+    def Read_Script(self, save_name: str):
+        save_path = os.path.join(self.script_path,f"{save_name.rsplit('.json',1)[0]}.json")
+
+        with open(save_path , "r") as file:
+            self.script_data = json.loads(file.read())
+
+        return self.script_data
