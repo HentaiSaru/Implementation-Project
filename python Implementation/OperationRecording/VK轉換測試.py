505d028102f1700794b79be050ca950e2130c3a5
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-28 23:57:36 +0800

    開發測試

---
+++ python Implementation/OperationRecording/VK轉換測試.py
@@ -0,0 +1,18 @@
+import win32api
+import win32con
+import time
+
+
+# 模擬按下 'a' 鍵
+# win32api.keybd_event(vk, 0, 0, 0)
+# win32api.keybd_event(vk, 0, win32con.KEYEVENTF_KEYUP, 0)
+
+table = "abcdefghijklmnopqrstuvwxyz"
+table2 = ['up','left',"down","right"]
+
+def test(key):
+    vk = win32api.VkKeyScan(key)
+    return vk
+
+for key in table2:
+    print(test(key))
