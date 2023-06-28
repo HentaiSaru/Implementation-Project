1754be6999a911cef5399d889ef0911f251d91bd
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-28 23:58:40 +0800

    錄製邏輯

---
+++ python Implementation/OperationRecording/Package/Operation_Logic/Operation_Recording.py
@@ -0,0 +1,105 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from pynput import keyboard , mouse
+from Record_Read import InputOutput
+from datetime import datetime
+import threading
+import time
+import re
+
+# 操作紀錄
+class Record:
+    def __init__(self):
+        self.io = InputOutput()
+        self.or_dict = {}
+        self.or_list = []
+
+        self.step = 0
+        self.ST = 0
+        self.ET = 0
+        self.Mlistener = None
+        self.Klistener = None
+
+        self.MouseMark = "M"
+        self.KeyboardMark = "K"
+
+    # 滑鼠錄製
+    def __Mouse_Record(self):
+        
+        # 點擊位置
+        def click(x, y, button, pressed):
+            if pressed:
+                self.ST = time.time()
+                self.step += 1
+            else:
+                self.ET = time.time()
+                self.or_list = [x, y, str(button).split("Button.")[1], (self.ET-self.ST)]
+                self.or_dict[f"{self.MouseMark}-{self.step}"] = self.or_list
+
+        # 滾動
+        def scroll(x, y, none, step):
+            self.step += 1
+            # 滾動的 step +1 是上滾輪 -1 是下滾輪
+            self.or_list = [x, y, step, 0]
+            self.or_dict[f"{self.MouseMark}-{self.step}"] = self.or_list
+
+        # 監聽
+        with mouse.Listener(on_click=click, on_scroll=scroll) as Mlistener:
+            self.Mlistener = Mlistener
+            time.sleep(0.01)
+            Mlistener.join()
+
+    # 鍵盤錄製 (不支援組合鍵/快捷鍵)
+    def __Keyboard_Record(self):
+        
+        # 按下
+        def press(key):
+            self.ST = time.time()
+            self.step += 1
+
+        # 放開
+        def release(key):
+            self.ET = time.time()
+            New_key = re.sub(r"[<>'\"]", "", str(key))
+            try:
+                New_key = New_key.split("Key.")[1]
+            except:
+                pass
+
+            self.or_list = [New_key , (self.ET-self.ST)]
+            self.or_dict[f"{self.KeyboardMark}-{self.step}"] = self.or_list
+
+        # 監聽
+        with keyboard.Listener(on_press=press, on_release=release) as Klistener:
+            self.Klistener = Klistener
+            time.sleep(0.01)
+            Klistener.join()
+
+    # 開始錄製
+    def Start_Record(self):
+        self.step = 0
+        self.or_dict.clear()
+
+        time.sleep(1) # 避免啟動快捷被錄製
+        print("開始錄製")
+
+        threading.Thread(target=self.__Mouse_Record).start()
+        threading.Thread(target=self.__Keyboard_Record).start()
+
+    # 中止錄製
+    def Stop_Record(self):
+        self.Mlistener.stop()
+        self.Klistener.stop()
+
+        print("結束錄製")
+
+        # 結束會自動輸出
+        self.__OutPut_Record()
+
+    def __OutPut_Record(self):
+        self.io.Save_Script(
+            f"Script-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}",
+            self.or_dict
+        )
