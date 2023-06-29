b222b841aed433d9a2d107edc95579307a22db0c
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-29 16:04:19 +0800

    主程式調整

--- python Implementation/OperationRecording/Main.py
+++ python Implementation/OperationRecording/Main.py
@@ -1,7 +1,9 @@
 from Package.Operation_Logic.ShortcutKey_Trigger import KeyboardMonitor
 
+"""
+* 回放結束 , 會直接中止程式
+"""
+
 keybo = KeyboardMonitor()
-
 hotkey = {"SR":["Alt","F2"],"ER":["Alt","F3"],"SP":["Ctrl","F1"],"EP":["Ctrl","F2"]}
-
-keybo(hotkey)
+keybo(hotkey , "Script-2023-06-29-16-00-13")
