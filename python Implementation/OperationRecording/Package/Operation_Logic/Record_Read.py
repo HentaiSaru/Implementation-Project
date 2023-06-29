1702ed9837c6a79e976a0ccdc28d164b5ab2212f
TenshinoOtoKafu <s3459897@gmail.com>
2023-06-29 16:05:10 +0800

    邏輯修正

--- python Implementation/OperationRecording/Package/Operation_Logic/Record_Read.py
+++ python Implementation/OperationRecording/Package/Operation_Logic/Record_Read.py
@@ -1,10 +1,15 @@
 import json
+import sys
 import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from StopCompletely import Exit
 
 #Todo - 腳本保存 與 腳本讀取 -
 
 class InputOutput:
     def __init__(self):
+        self.exit = Exit()
         self.script_path = "{}\\Script".format(os.path.dirname(os.path.abspath(__file__)).rsplit("\\",2)[0])
         self.script_data = {}
 
@@ -18,10 +23,18 @@ class InputOutput:
         with open(save_path , "w") as file:
             file.write(json.dumps(save_data))
 
+        print(f"{save_name}.json 腳本以保存")
+
     def Read_Script(self, save_name: str):
         save_path = os.path.join(self.script_path,f"{save_name.rsplit('.json',1)[0]}.json")
 
-        with open(save_path , "r") as file:
-            self.script_data = json.loads(file.read())
+        try:
 
-        return self.script_data
+            with open(save_path , "r") as file:
+                self.script_data = json.loads(file.read())
+
+            return self.script_data
+        
+        except:
+            print("找不到設置腳本\n強制中止程式")
+            self.exit.ex()
