a2c4d69d922fef4f7edddbc0033677c2e2dfd397
TenshinoOtoKafu <s3459897@gmail.com>
2023-07-05 00:42:05 +0800

    Jkf功能微調

--- python Implementation/AutomatedActionScript/DailyCheckinScript.py
+++ python Implementation/AutomatedActionScript/DailyCheckinScript.py
@@ -160,12 +160,18 @@ class forum:
         jkfdriver.get("https://www.jkforum.net/material/my_item")
         jkfdriver.execute_script('Object.defineProperty(navigator, "webdriver", {get: () => undefined})')
 
-        time.sleep(15) # 這網站加載很慢,等久一點(運行失敗就再加長時間)
+        try:
+            WebDriverWait(jkfdriver,30).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='router-link-active router-link-exact-active block lv-2-tab']")))
+        except:
+            input("等待登入帳號...\n(按下Enter) :")
+            os.system("cls")
+            jkfdriver.get("https://www.jkforum.net/material/my_item")
+            jkfdriver.execute_script('Object.defineProperty(navigator, "webdriver", {get: () => undefined})')
+        
         Content = jkfdriver.page_source.encode('utf-8').strip()
         html = BeautifulSoup(Content,'html.parser')
 
         try: # 使用小型體力藥水
-        
             smallpotion = WebDriverWait(jkfdriver,20).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'item-wrap') and .//div[contains(text(), '小型體力藥水')]]//button[contains(text(), '查看')]")))
             smallpotion.click()
 
@@ -219,15 +225,17 @@ class forum:
         startmining =  WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH, f"//div[@class='pt-10'][{Location}] //div[@class='OvtXIlmLtXEE_eWpy1jH YOKk3zC9K8EXQZUZPFiy'][text()='開始挖礦']")))
         startmining.click()
 
-        # 先點選5次畫布,因為使用相對位置找不到,所以用絕對位置,可能之後需要修改
-        mining = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div[2]/div[2]/div/div[2]/div/div[2]/canvas")))
-        for _ in range(5):jkfdriver.execute_script("arguments[0].click();", mining)
-        
-        # 按再一次
-        for _ in range(Quantity-1): # 因為上面執行過一遍,這邊-1
-            time.sleep(0.1)
-            again = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='OvtXIlmLtXEE_eWpy1jH'][text()='再挖一次']")))
-            again.click()
+        try:
+            # 先點選5次畫布,因為使用相對位置找不到,所以用絕對位置,可能之後需要修改
+            mining = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div[2]/div[2]/div/div[2]/div/div[2]/canvas")))
+            for _ in range(5):jkfdriver.execute_script("arguments[0].click();", mining)
+
+            # 按再一次
+            for _ in range(Quantity-1): # 因為上面執行過一遍,這邊-1
+                time.sleep(0.1)
+                again = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='OvtXIlmLtXEE_eWpy1jH'][text()='再挖一次']")))
+                again.click()
+        except:pass
 
         time.sleep(Sc)
         pickle.dump(jkfdriver.get_cookies(), open("./Jkfdefault/JkfCookies.pkl","wb"))
@@ -253,15 +261,17 @@ class forum:
         startexplore = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH, f"//div[@class='pt-10'][{Location}] //div[@class='OvtXIlmLtXEE_eWpy1jH YOKk3zC9K8EXQZUZPFiy'][text()='開始探索']")))
         startexplore.click()
 
-        explore = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div[2]/div[2]/div/div[2]/div/div[2]/canvas")))
-        for _ in range(5):jkfdriver.execute_script("arguments[0].click();", explore)
+        try:
+            explore = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div[2]/div[2]/div/div[2]/div/div[2]/canvas")))
+            for _ in range(5):jkfdriver.execute_script("arguments[0].click();", explore)
 
-        for _ in range(Quantity-1): # 因為上面執行過一遍,這邊-1
-            time.sleep(0.1)
-            again = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='OvtXIlmLtXEE_eWpy1jH'][text()='再挖一次']")))
-            again.click()
-            # 如果被NPC元素擋到按鈕元素,可以使用JS的點擊
-            #jkfdriver.execute_script("arguments[0].click();", again)
+            for _ in range(Quantity-1): # 因為上面執行過一遍,這邊-1
+                time.sleep(0.1)
+                again = WebDriverWait(jkfdriver,10).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='OvtXIlmLtXEE_eWpy1jH'][text()='再挖一次']")))
+                again.click()
+                # 如果被NPC元素擋到按鈕元素,可以使用JS的點擊
+                #jkfdriver.execute_script("arguments[0].click();", again)
+        except:pass
 
         time.sleep(Sc)
         pickle.dump(jkfdriver.get_cookies(), open("./Jkfdefault/JkfCookies.pkl","wb"))
@@ -446,7 +456,6 @@ class script:
         checkin = WebDriverWait(StarRail,60).until(EC.element_to_be_clickable((By.XPATH, f"//div[@class='components-pc-assets-__prize-list_---item---F852VZ']/span[@class='components-pc-assets-__prize-list_---no---3smN44'][contains(text(), '第{checkinday}天')]")))
         
         checkin.click()
-        #StarRail.execute_script("arguments[0].click();", checkin)
 
         time.sleep(Sc)
         GetParametric().datacreation(StarRail.get_cookies(),GetParametric().databases("StarRail"),"StarRailCookies")
@@ -483,19 +492,19 @@ if __name__ == "__main__":
     threading.Thread(target=script.Open_StarRail,args=(10,)).start()
 
     """反覆操作預計之後使用scapy進行封包修改操作"""
-    # Jkf論壇使用體力藥水(此腳本就是藥水全部都用完)
-    #forum.jkf_use_props(5)
+    # Jkf論壇使用體力藥水(此腳本就是藥水全部都用完) Sc = 結束後等待幾秒關閉
+    # forum.jkf_use_props(5)
 
     # Jkf論壇自動挖礦(次數,地點,運行完停留時間)
     # 地點 : "巨龍巢穴" "精靈峽谷" "廢棄礦坑"
-    #forum.jkf_mining(10,"廢棄礦坑",5)
+    # forum.jkf_mining(10,"廢棄礦坑",5)
 
     # Jkf論壇自動探索(次數,地點,運行完停留時間)
     # 地點 : "墮落聖地" "焚燒之地" "巨木森林"
-    #forum.jkf_explore(10,"巨木森林",5)
+    # forum.jkf_explore(10,"巨木森林",5)
 
     # 輸出Cookie內容的方法 資料位置 , 要開啟的Cookie檔案名
-    #GetParametric().CookieView("blackdefault","blackCookies")
+    # GetParametric().CookieView("blackdefault","blackCookies")
 
     # 刪除 selenium 非正常關閉時,的遺留資料夾
     # TrashRemoval()
