5961ce9070faef81b75f30f7dc77378e1cf824b7
TenshinoOtoKafu <s3459897@gmail.com>
2023-07-06 17:19:32 +0800

    新增簡易隨機Port

--- python Implementation/AutomationScript/Version_2.0/Script/Parameters.py
+++ python Implementation/AutomationScript/Version_2.0/Script/Parameters.py
@@ -15,7 +15,11 @@ class Parameters:
         self.generate_port = []
         self.port = 1024
     
-    # 隨機端口
+    """
+    * 隨機端口生成
+    * 二種版本再低於 2 萬以下的生成量 , 差異並不大
+    """
+    # 複雜版-隨機端口 (複雜版當生成量大時 , 所需時間更長) [懷舊用]
     def RandomPort(self):
         increment_port = int(self.port * random.uniform(1.0, 64.0))
         
@@ -33,18 +37,32 @@ class Parameters:
                     raise Exception()
             except:
                 return self.RandomPort()
+    
+    # 簡易版-隨機端口 (簡易版生成量大時 , 時間只需複雜版的一半)
+    def Simple_RandomPort(self):
+        port = random.randint(self.port, 65535)
+        if port not in self.generate_port:
+            self.generate_port.append(port)
+            return port
+        else:
+            return self.Simple_RandomPort()
             
     # 計算等待秒數
     def WaitingTime(self):
+        # 取得時區 (亞洲/台北)
         TaipeiTimezone = pytz.timezone('Asia/Taipei')
-        TargetTime = datetime.time(hour=0, minute=0, second=0)
+        # 當前時區時間
         current_time = datetime.datetime.now(TaipeiTimezone).time()
+        
+        # 設置目標時間 , 00:00 分
+        TargetTime = datetime.time(hour=0, minute=0, second=0)
         
         if current_time < TargetTime:
             seconds_to_wait = (datetime.datetime.combine(datetime.date.today(), TargetTime) - datetime.datetime.combine(datetime.date.today(), current_time)).seconds
         else:
             seconds_to_wait = (datetime.datetime.combine(datetime.date.today() + datetime.timedelta(days=1), TargetTime) - datetime.datetime.combine(datetime.date.today(), current_time)).seconds
         
+        # 當等待時間 > 1 分鐘 , 回傳 3 秒
         if seconds_to_wait > 60:
             return 3
         else:
