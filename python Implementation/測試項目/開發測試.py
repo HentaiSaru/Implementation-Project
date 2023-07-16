3a9c7db502904ecf92e9f99fae153718dc8793d9
TenshinoOtoKafu <s3459897@gmail.com>
2023-07-16 18:34:36 +0800

    測試變更

--- python Implementation/測試項目/開發測試.py
+++ python Implementation/測試項目/開發測試.py
@@ -1,5 +1,6 @@
 from unicodedata import normalize
 import requests
+import keyboard
 
 session = requests.Session()
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
@@ -7,9 +8,5 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 url = ""
 filterdomains=["google.com","bing.com","youtube.com","facebook.com","line.me","sharepoint.com","taobao.com","shopee.tw"]
 
-data = session.get(url , headers=headers)
-
 # if data.url == url:
     # print("錯誤的")
-
-print(data.text)
