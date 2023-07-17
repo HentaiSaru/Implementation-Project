d9df6e23e08478e81a488f89d5be38c100c144af
TenshinoOtoKafu <s3459897@gmail.com>
2023-07-17 11:33:42 +0800

    開發測試

--- python Implementation/測試項目/開發測試.py
+++ python Implementation/測試項目/開發測試.py
@@ -1,6 +1,8 @@
 from unicodedata import normalize
 import requests
 import keyboard
+import threading
+import time
 
 session = requests.Session()
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
