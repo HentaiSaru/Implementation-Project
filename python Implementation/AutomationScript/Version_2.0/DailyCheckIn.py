47b0939e6428e03613243f2f0e435e62c55b8101
TenshinoOtoKafu <s3459897@gmail.com>
2023-12-13 10:47:12 +0800

    縮排修正

--- python Implementation/AutomationScript/Version_2.0/DailyCheckIn.py
+++ python Implementation/AutomationScript/Version_2.0/DailyCheckIn.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from Script.Parameters import paramet
-from Script.Dataio import DO , DI
+from Script.Dataio import DO, DI
 from selenium import webdriver
 from lxml import etree
 import threading
