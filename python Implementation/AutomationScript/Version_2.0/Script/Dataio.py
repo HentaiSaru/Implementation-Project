895fe5fde6b8e86dfd4f8ac359d29c62bf41d0d0
TenshinoOtoKafu <s3459897@gmail.com>
2023-12-13 10:47:34 +0800

    縮排修正

--- python Implementation/AutomationScript/Version_2.0/Script/Dataio.py
+++ python Implementation/AutomationScript/Version_2.0/Script/Dataio.py
@@ -19,27 +19,27 @@ def output_json(Json_name, Json_data):
 class DataImport:
     def __init__(self):
         self.path = data_location()
-    
+
     def get_website_data(self, web: str):
         data_path = os.path.join(self.path, f"{web}_default")
-        
+
         if os.path.exists(data_path):
             return data_path
         else:
             self.create_folder(data_path)
             return data_path
-            
+
     def get_website_cookie(self, web: str):
         data_path = os.path.join(self.path, f"{web}_default\\Jkf_cookies.json")
-        
+
         if os.path.exists(data_path):
             return import_json(data_path)
         else:
             return None
-        
+
     def get_acc(self):
         data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Account.json")
-        
+
         if os.path.exists(data_path):
             Account = import_json(data_path)
             return Account
@@ -61,22 +61,22 @@ class DataOutput:
     def __init__(self):
         self.path = data_location()
         self.cookie_save = []
-    
+
     def json_cookie(self, cookies: dict, web: str):
         self.cookie_save.clear()
         data_path = os.path.join(self.path, f"{web}_default")
-        
+
         for cookie in cookies:
             cookie_dict = {}
             cookie_dict["name"] = cookie["name"]
             cookie_dict["value"] = cookie["value"]
             self.cookie_save.append(cookie_dict)
-            
+
         output_json(f"{data_path}\\{web}_cookies.json", self.cookie_save)
-        
+
     def pkl_cookie(self, cookies: dict, web: str):
         data_path = os.path.join(self.path, f"{web}_default")
         pickle.dump(cookies, open(f"{data_path}\\{web}_cookies.pkl","wb"))
-    
+
 DI = DataImport()
 DO = DataOutput()
