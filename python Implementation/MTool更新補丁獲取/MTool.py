dfa875b1c9d4a329275e2ef1a2cb2f9f4117c988
TenshinoOtoKafu <s3459897@gmail.com>
2024-01-10 16:05:18 +0800

    修正可用性

--- python Implementation/MTool更新補丁獲取/MTool.py
+++ python Implementation/MTool更新補丁獲取/MTool.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from lxml import etree
 import requests
 import os
@@ -6,10 +7,10 @@ class UpdateDetection:
     def __init__(self):
         self.url = "https://trs.mtool.app/release.php?lang=chs"
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-            "Cookie":"7F2210D0_inc=1; supportKey=00000000000000000000000000000000; from=7F2210D0",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Cookie":"supportKey=00000000000000000000000000000000; from=66D39A34",
+            "Cache-Control":"no-cache",
             "Host":"trs.mtool.app",
-            "Referer":"https://afdian.net/",
         }
         self.session = requests.Session()
 
@@ -21,31 +22,29 @@ class UpdateDetection:
 
     def Get_page_data(self):
         request = self.session.get(self.url, headers=self.headers)
-        return etree.fromstring(request.content, etree.HTMLParser())
+        return etree.HTML(request.text)
     
     def Data_processing(self):
         tree = self.Get_page_data()
+
         # 版本號
-        self.version = tree.xpath("//h4/text()")[1].split(": ")[1].strip()
+        title = tree.xpath("//h4")[0]
+        self.version = "Version : {}{}".format(
+            title.xpath("./text()")[0].split(": ")[1],
+            datetime.fromtimestamp(int(title.xpath("./span/text()")[0])).strftime("%Y-%m-%d %H:%M:%S")
+        )
         # 更新補丁
-        self.update_patch = f"https://trs.mtool.app/{tree.xpath('//p/a')[2].get('href')}"
-        # 完整補丁
-        self.full_patch = f"https://trs.mtool.app/{tree.xpath('//p/a')[3].get('href')}"
+        self.update_patch = "https://trs.mtool.app/{}".format(tree.xpath('//a[@class="goAfterClick"]/@href')[0])
 
     def Get_version(self):
         print(self.version)
 
+    # 啟動下載更新補丁
     def Get_UP(self):
         os.system(f"start {self.update_patch}")
-
-    def Get_FP(self):
-        os.system(f"start {self.full_patch}")
 
 if __name__ == "__main__":
     check = UpdateDetection()
 
-    # 獲取更新連結
+    check.Get_version()
     check.Get_UP()
-
-    # 獲取完整連結 (有BUG , 確定取得是完整下載連結 , 但實際上只下載更新補丁)
-    # check.Get_FP()
