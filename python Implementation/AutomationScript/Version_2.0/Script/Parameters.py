ccdf588cdc3ff19ce2dbf73a602a0420ae93a3dc
TenshinoOtoKafu <s3459897@gmail.com>
2024-02-06 15:12:20 +0800

    修改函數

--- python Implementation/AutomationScript/Version_2.0/Script/Parameters.py
+++ python Implementation/AutomationScript/Version_2.0/Script/Parameters.py
@@ -67,9 +67,10 @@ class Parameters:
         else:
             return seconds_to_wait
 
-    # 參數設置
-    def AddSet(self, value: str, headless: bool = False):
+    # 參數設置 (傳入的 value, 會去找同名的數據檔案, 除非去設置 UserData 的位置)
+    def AddSet(self, value: str, headless: bool = False, UserData: str = ""):
         if headless:self.Settings.add_argument("--headless")
+        if UserData == "": UserData = DI.get_website_data(value)
         self.Settings.add_argument("--no-sandbox")
         self.Settings.add_argument("--log-level=3")
         self.Settings.add_argument("--start-maximized")
@@ -78,8 +79,8 @@ class Parameters:
         self.Settings.add_argument("--ignore-certificate-errors")
         self.Settings.add_argument("--remote-debugging-address=0.0.0.0")
         self.Settings.add_argument(f"--remote-debugging-port={self.RandomPort()}")
-        self.Settings.add_argument(f"--user-data-dir={DI.get_website_data(value)}")
-        self.Settings.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+        self.Settings.add_argument(f"--user-data-dir={UserData}")
+        self.Settings.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36")
 
         self.Settings.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.Settings.add_experimental_option('excludeSwitches', ['enable-automation'])
