c9ebcb6e59457c232afe2acafc5dbee67f637336
TenshinoOtoKafu <s3459897@gmail.com>
2024-04-20 05:21:32 +0800

    調整

--- python Implementation/漫畫下載器/測試版本/Script/GetCookiesAutomatically.py
+++ python Implementation/漫畫下載器/測試版本/Script/GetCookiesAutomatically.py
@@ -1,4 +1,5 @@
-import undetected_chromedriver as uc
+from selenium.webdriver.support.ui import WebDriverWait
+import undetected_chromedriver2 as uc
 import random
 import json
 import time
@@ -18,7 +19,6 @@ class AutomationRequest:
         self.driver = None
         self.cookie = {}
         self.timeout = 0
-        self.hidden = None
 
         self.show = "."
         self.load = 0
@@ -32,10 +32,10 @@ class AutomationRequest:
         print("重置中請稍後...")
         # 關閉 Google
         os.system('wmic process where name="chrome.exe" delete >nul 2>&1')
-        # 刪除 undetected_chromedriver
-        os.system("pip uninstall undetected_chromedriver -y >nul 2>&1")
-        # 安裝 undetected_chromedriver
-        os.system("pip install undetected_chromedriver >nul 2>&1")
+        # 刪除 undetected_chromedriver2
+        os.system("pip uninstall undetected_chromedriver2 -y >nul 2>&1")
+        # 安裝 undetected_chromedriver2
+        os.system("pip install undetected_chromedriver2 >nul 2>&1")
         # 更新 Python 和 pip
         os.system("python.exe -m pip install --upgrade pip >nul 2>&1")
         os.system("pip install --upgrade setuptools >nul 2>&1")
@@ -43,11 +43,11 @@ class AutomationRequest:
         print("重置完成...")
 
     def __Setting_Options(self):
-        if self.hidden:self.Settings.add_argument("--headless")
         self.Settings.add_argument("--incognito")
         self.Settings.add_argument('--no-sandbox')
         self.Settings.add_argument('--log-level=3')
         self.Settings.add_argument('--no-first-run')
+        self.Settings.add_argument("--headless=new")
         self.Settings.add_argument('--disable-infobars')
         self.Settings.add_argument("--disable-extensions")
         self.Settings.add_argument('--no-service-autorun')
@@ -63,7 +63,7 @@ class AutomationRequest:
         self.Settings.add_argument("--disable-plugins-discovery")
         self.Settings.add_argument('--remote-debugging-address=0.0.0.0')
         self.Settings.add_argument('--disable-blink-features=AutomationControlled')
-        self.Settings.add_argument(f"--remote-debugging-port={random.randint(1024,65535)}")
+        self.Settings.add_argument(f"--remote-debugging-port={random.randint(1024, 65535)}")
         return self.Settings
 
     def __Loading_Display(self, time: int):
@@ -77,12 +77,19 @@ class AutomationRequest:
 
     def __Get_Settings(self):
         return Chrome(
-            version_main=116,
+            version_main=123,
+            user_multi_procs=True,
+            advanced_elements=True,
             options=self.__Setting_Options(),
             driver_executable_path=self.driver_path
         )
+        
+    def __LoadWait(self):
+        WebDriverWait(self.driver, 10).until(
+            lambda driver: driver.execute_script("return document.readyState") == "complete"
+        )
     
-    def AGCookie(self,url: str,json: str):
+    def AGCookie(self, url: str, json: str):
         """
     自動請求 Cookie
     >>> 參數
@@ -96,21 +103,22 @@ class AutomationRequest:
     *   預設有 30 秒的超時時間 , 超過這時間沒有請求到 , 將會回傳 False
         """
         print("嘗試獲取 Cookie ==>")
-        self.hidden = True
         try:
             if json.find(".json") != -1:
                 json = json.rsplit(".", 1)[0]
 
             self.driver = self.__Get_Settings()
             self.driver.get(url)
+            self.__LoadWait()
+            self.driver.execute_script('Object.defineProperty(navigator, "webdriver", {get: () => undefined})')
 
             while True:
-                cookies = self.driver.get_cookies()    
+                cookies = self.driver.get_cookies()
                 for index in range(len(cookies)):
                     name = cookies[index]['name']
                     value = cookies[index]["value"]
                     self.cookie[name] = value
-                    
+
                 if len(self.cookie) > 1:
                     self.__OutputCookie(json)
                     return True
@@ -121,13 +129,13 @@ class AutomationRequest:
                     # 超時 30 秒退出
                     if self.timeout > 30: 
                         raise Exception()
-                    
+
                 self.__Loading_Display(self.timeout)
-        except:
+        except Exception as e:
             self.driver.close()
             return False
 
-    def MGCookie(self,url: str,json: str):
+    def MGCookie(self, url: str, json: str):
         """
     手動請求 Cookie
     >>> 參數
@@ -141,16 +149,16 @@ class AutomationRequest:
     *   該窗口網站並不會記錄登入狀態 , 所以每次呼叫都要重登
     """
         print("啟動窗口等待獲取 cookie ==>")
-        self.hidden = False
         try:
             if json.find(".json") != -1:
                 json = json.rsplit(".", 1)[0]
 
             self.driver = self.__Get_Settings()
             self.driver.get(url)
+            self.__LoadWait()
+            self.driver.execute_script('Object.defineProperty(navigator, "webdriver", {get: () => undefined})')
 
             while True:
-
                 confirm = input("請[登入帳號後]鍵入 (y) 進行獲取 : ")
 
                 if confirm == "y":
@@ -165,17 +173,17 @@ class AutomationRequest:
                         self.__OutputCookie(json)
                     else:
                         return False
-                    
+
                     self.driver.close()
                     break
                 else:
                     print("請輸入正確的確認鍵\n")
                     continue
-        except:
+        except Exception as e:
             return False
 
     def __OutputCookie(self,name):
         with open(f"{name}.json" , "w") as file:
-            file.write(json.dumps(self.cookie, indent=4, separators=(',',':')))
+            file.write(json.dumps(self.cookie, indent=4, separators=(',', ':')))
  
 Get = AutomationRequest()
