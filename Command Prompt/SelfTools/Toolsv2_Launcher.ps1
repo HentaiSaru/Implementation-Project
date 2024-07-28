c183d622a22dc733b3a104d1341f166f1cb9ab2f
TenshinoOtoKafu <s3459897@gmail.com>
2024-07-28 17:41:56 +0800

    新增例外時的預設值

--- Command Prompt/SelfTools/Toolsv2_Launcher.ps1
+++ Command Prompt/SelfTools/Toolsv2_Launcher.ps1
@@ -3,41 +3,40 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force # 給予臨時
 Add-Type -AssemblyName System.Windows.Forms
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
-# 字串操作物件
 $String = @{
     ToMD5 = {
-        param (
-            [string]$string,
-            [int]$byte = 32
-        )
+        param ([string]$string, [int]$byte = 32)
+        try {
+            $md5 = [System.Security.Cryptography.MD5]::Create()
+            $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($string)
+            $hashBytes = $md5.ComputeHash($fileBytes)
+            $hashString = [BitConverter]::ToString($hashBytes) -replace '-'
+            $lowerHash = $hashString.ToLower()
 
-        $MD5 = [System.Security.Cryptography.MD5]::Create()
-        $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($string)
-        $hashBytes = $MD5.ComputeHash($fileBytes)
-        $hashString = [BitConverter]::ToString($hashBytes) -replace '-'
-        $lowerHash = $hashString.ToLower()
-
-        return $lowerHash.Substring(0, [System.Math]::Min($byte, 32))
+            return $lowerHash.Substring(0, [System.Math]::Min($byte, 32))
+        } catch {
+            return "cfc492e970cbd70a9fd8f94b15432d17"
+        } 
     };
     ToSHA = {
-        param (
-            [string]$string,
-            [int]$byte = 64
-        )
+        param ([string]$string, [int]$byte = 64)
+        try {
+            $sha256 = [System.Security.Cryptography.SHA256]::Create()
+            $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($string)
+            $hashBytes = $sha256.ComputeHash($fileBytes)
+            $hashString = [BitConverter]::ToString($hashBytes) -replace '-'
+            $lowerHash = $hashString.ToLower()
 
-        $sha256 = [System.Security.Cryptography.SHA256]::Create()
-        $fileBytes = [System.Text.Encoding]::UTF8.GetBytes($string)
-        $hashBytes = $sha256.ComputeHash($fileBytes)
-        $hashString = [BitConverter]::ToString($hashBytes) -replace '-'
-        $lowerHash = $hashString.ToLower()
-
-        return $lowerHash.Substring(0, [System.Math]::Min($byte, 64))
+            return $lowerHash.Substring(0, [System.Math]::Min($byte, 64))
+        } catch {
+            return "08b7eefdb5072d28c60bb71daa3d59176d5a9060f1821c29ca0cf3cafb30835d"
+        }
     }
 }
 
 function Print {
     param (
-        [string]$text,
+        [string]$text = "",
         [string]$foregroundColor = 'White',
         [string]$backgroundColor = 'Black'
     )
@@ -48,7 +47,7 @@ function Print {
 
 function CheckNetwork { # 檢查網路連接
     try {
-        Test-Connection -ComputerName "1.1.1.1" -Count 1 -ErrorAction Stop
+        Test-Connection -ComputerName "8.8.8.8" -Count 1 -ErrorAction Stop
         return $true
     } catch {
         return $false
@@ -126,21 +125,23 @@ class ProcessingCore {
     }
 }
 
-<# ============== #>
+Print "============= 檢查更新 =============" "Yellow"
 
-Print "======== 檢查更新中 ========" "Yellow"
+$InfoHash = $null
+try {
+    # 取得使用者電腦資訊
+    $BiosInfo = Get-WmiObject -Class Win32_BIOS | Select-Object -Property SerialNumber
+    $BaseBoard = Get-WmiObject -Class Win32_BaseBoard | Select-Object -Property Product, SerialNumber
+    $UserInfo = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property PrimaryOwnerName, Name
+    $InfoHash = &($String.ToMD5) "$($UserInfo.PrimaryOwnerName)$($UserInfo.Name)$($BiosInfo.SerialNumber)$($BaseBoard.Product)$($BaseBoard.SerialNumber)"
+} catch {
+    $InfoHash = &($String.ToMD5) "Author: Canaan HS"
+}
 
-# 取得使用者電腦資訊
-$BiosInfo = Get-WmiObject -Class Win32_BIOS | Select-Object -Property SerialNumber
-$BaseBoard = Get-WmiObject -Class Win32_BaseBoard | Select-Object -Property Product, SerialNumber
-$UserInfo = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property PrimaryOwnerName, Name
-
-# 已使用者的電腦資訊計算 MD5 後, 分別取前半和後半作為 key,iv
-$InfoHash = &($String.ToMD5) "$($UserInfo.PrimaryOwnerName)$($UserInfo.Name)$($BiosInfo.SerialNumber)$($BaseBoard.Product)$($BaseBoard.SerialNumber)"
 $KeyHash = $InfoHash.Substring(0, 16)
 $IvHash = $InfoHash.Substring(16, 16)
 
-# 使用者資訊的哈希值, 合併成 保存目錄路徑
+# 資訊哈希值, 合併成 保存目錄路徑
 $LocalFile = "$env:Temp\$InfoHash"
 $FileExists = {return Test-Path $LocalFile}
 $DownloadURL = "https://raw.githubusercontent.com/Canaan-HS/Implementation-Project/Main/Command%20Prompt/SelfTools/Toolsv2.ps1"
@@ -154,8 +155,7 @@ $Core = [ProcessingCore]::new(
 
 if (-not (CheckNetwork)) { # 沒有網路
     $Message = [System.Windows.Forms.MessageBox]::Show(
-        "無法獲取更新, 是否嘗試啟動本地文件",
-        "沒有網路",
+        "無法獲取更新, 是否嘗試啟動本地文件", "沒有網路",
         [System.Windows.Forms.MessageBoxButtons]::OKCancel,
         [System.Windows.Forms.MessageBoxIcon]::Information
     )
@@ -165,8 +165,7 @@ if (-not (CheckNetwork)) { # 沒有網路
         $Core.InvokeCode($Core.GetDecrypt())
     } else {
         $Message = [System.Windows.Forms.MessageBox]::Show(
-            "本地無啟動文件",
-            "找不到文件",
+            "本地無啟動文件", "找不到文件",
             [System.Windows.Forms.MessageBoxButtons]::OK,
             [System.Windows.Forms.MessageBoxIcon]::Error
         )
@@ -182,8 +181,7 @@ if (-not (CheckNetwork)) { # 沒有網路
             continue
         } elseif ($remoteString -eq "Update address to change") {
             $Message = [System.Windows.Forms.MessageBox]::Show(
-                "更新地址已變更嘗試已本地文件開啟",
-                "地址已變更",
+                "更新地址已變更 將嘗試開啟本地文件", "地址已變更",
                 [System.Windows.Forms.MessageBoxButtons]::OK,
                 [System.Windows.Forms.MessageBoxIcon]::Warning
             )
@@ -210,8 +208,7 @@ if (-not (CheckNetwork)) { # 沒有網路
         $codeString = $Core.GetDecrypt()
     } else {
         $Message = [System.Windows.Forms.MessageBox]::Show(
-            "本地無啟動文件",
-            "找不到文件",
+            "本地無啟動文件", "找不到文件",
             [System.Windows.Forms.MessageBoxButtons]::OK,
             [System.Windows.Forms.MessageBoxIcon]::Error
         )
