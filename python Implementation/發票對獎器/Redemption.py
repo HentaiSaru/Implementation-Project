09cb278eacc3ffed76f090c16c1a8ff2b2dab32c
TenshinoOtoKafu <s3459897@gmail.com>
2024-08-13 09:41:14 +0800

    1. 複寫原生打印 2. 改寫部份數據結構 3. 修改部份實現邏輯

--- python Implementation/發票對獎器/Redemption.py
+++ python Implementation/發票對獎器/Redemption.py
@@ -1,12 +1,21 @@
-from lxml import etree
-import threading
+# import threading
 import msvcrt
-import httpx
-import time
+# import time
 import json
 import os
 
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.console import Console
+from rich.table import Table
+from lxml import etree
+import httpx
+
 #! 預計添加: 根據獲取月份, 顯示兌獎期限, 並修改 dev 也就是獲取時顯示的數據, 根據取得區域顯示對應獎項
+
+# 複寫原生打印方式
+console = Console()
+def print(*args, **kwargs):
+    console.print(*args, **kwargs)
 
 class WinningInstructions:
     def __init__(self) -> None:
@@ -30,17 +39,16 @@ class WinningInstructions:
 
 class DataProcessing:
     def __init__(self) -> None:
+        self.Redemption_Data = None
         self.client = httpx.Client(http2=True)
-        self.Url = "https://invoice.etax.nat.gov.tw/index.html"
         self.Headers = {
             "Cache-Control": "no-cache",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
         }
-        self.Redemption_Data = None
 
-    def __Get_Data(self, Url) -> etree.Element:
+    def __Get_Data(self, Uri) -> etree.Element:
         try:
-            data = self.client.get(Url, headers=self.Headers)
+            data = self.client.get(Uri, headers=self.Headers)
             if data.status_code == 200:
                 return etree.HTML(data.text)
             else:
@@ -54,40 +62,50 @@ class DataProcessing:
         number = tree.xpath("//table[@class='etw-table-bgbox etw-tbig']/tbody[1]")[0].xpath(".//span/text()")
         return number[0:2] + [f'{number[i]}{number[i + 1]}' for i in range(2, len(number), 2)]
 
-    def Data_Analysis(self):
-        link_Data = []
-        url = self.Url.rsplit("/", 1)[0]
-        tree = self.__Get_Data(self.Url)
+    def Data_Analysis(self, Uri) -> None:
+        link_Data = {}
+        Url = Uri.rsplit("/", 1)[0]
+        tree = self.__Get_Data(Uri)
 
         for tr in tree.xpath("//ul[@class='etw-submenu etw-submenu01']/li"):
             title = tr.xpath("./a")[0]
             href = title.get("href")
-            
+
             if href == "index.html": # 最近期
-                link_Data.append({title.get("title") : self.__Get_Number(tree)})
+                link_Data[1] = {
+                    "month": title.get("title"),
+                    "number": self.__Get_Number(tree)
+                }
+
             elif href == "lastNumber.html": # 上一期
-                link_Data.append({title.get("title") : self.__Get_Number(
-                    self.__Get_Data(f"{url}/{title.get('href')}")
-                )})
+                link_Data[2] = {
+                    "month": title.get("title"),
+                    "number": self.__Get_Number(
+                        self.__Get_Data(f"{Url}/{title.get('href')}")
+                    )
+                }
 
         self.Redemption_Data = link_Data
 
 class Comparison(DataProcessing, WinningInstructions):
-    def __init__(self) -> None:
+    def __init__(self, Uri) -> None:
         DataProcessing.__init__(self)
         WinningInstructions.__init__(self)
+
+        self.Uri = Uri
         self.input = ""
         self.winning = {}
-        self.bar = "░" * 10
-        self.wait = "兌獎號碼獲取中 "
-        self.space = " " * (len(self.bar) * len(self.wait))
 
-    def __Comparison_Date(self):
+        # self.bar = "░" * 10
+        # self.wait = "兌獎號碼獲取中 "
+        # self.space = " " * (len(self.bar) * len(self.wait))
+
+    def __Comparison_Date(self) -> None:
         print("\n")
 
         while True:
             if self.input == "":
-                print("輸入發票末三碼 [Esc 停止]: ", end="")
+                print("輸入發票末三碼 [Esc 停止]: ", end="", style="yellow")
 
             Input = msvcrt.getch().decode()
             if Input == "\x1b":
@@ -102,7 +120,7 @@ class Comparison(DataProcessing, WinningInstructions):
                     print("\n")
 
                     if self.input.lower() == "dev":
-                        print(f"{json.dumps(self.winning, indent=4, ensure_ascii=False)}\n")
+                        print(f"{json.dumps(self.winning, indent=4, ensure_ascii=False)}\n", style="bold bright_cyan")
 
                     elif not self.input.isnumeric():
                         self.input = ""
@@ -111,19 +129,28 @@ class Comparison(DataProcessing, WinningInstructions):
                     else:
                         winning = self.winning.get(self.input)
                         if winning is not None:
-                            print(f"自行確認是否中獎 ({winning['level']}): {winning['number']}\n")
+                            if winning['level'] == "頭獎":
+                                print(
+                                    f"中獎了!! 自行確認中獎等級 ({winning['level']}): {winning['number']}\n",
+                                    style="bold green"
+                                )
+                            else:
+                                print(
+                                    f"自行確認是否中獎 ({winning['level']}): {winning['number']}\n",
+                                    style="bold green"
+                                )
 
                     self.input = ""
 
             except ValueError:
                 print("錯誤輸入類型 !!\n")
-            except Exception:
-                print("幹啥呢 !!\n")
+            except Exception as E:
+                print(f"幹啥呢 !!\n")
                 break
 
-    def Select_Date(self) -> None:
-        threading.Thread(target=self.Data_Analysis).start()
-
+    def __Select_Date(self) -> None:
+        """ (原生進度條實現)
+        threading.Thread(target=self.Data_Analysis, args=(self.Uri,)).start()
         while self.Redemption_Data is None:
             print(self.wait, end="")
 
@@ -133,40 +160,59 @@ class Comparison(DataProcessing, WinningInstructions):
                 time.sleep(0.1)
 
             print(f"\r{self.space}\r", end="")
+        """
 
-        print("{:<6} {}".format("代號", "兌獎日期"))
-        for index, Redemption_Data in enumerate(self.Redemption_Data):
-            #! 如需要根據選取月份, 顯示兌換日期, 需要解析此處的 key, 選擇的日期
-            for key in Redemption_Data.keys():
-                print("[{}]{:<5} {}".format(index, "", key))
+        with Progress(SpinnerColumn(), TextColumn("[progress.description]{task.description}")) as progress:
+            task = progress.add_task("取得數據中...", start=False)
+            self.Data_Analysis(self.Uri)
+            progress.stop_task(task)
 
-        length = len(self.Redemption_Data)
+        os.system("cls")
+
+        table = Table()
+        table.add_column("代號", justify="center", style="bold bright_red")
+        table.add_column("日期", justify="center", style="bold bright_yellow")
+
+        for index, data in self.Redemption_Data.items():
+            table.add_row(str(index), data["month"])
+
+        print(table)
+
         while True:
             try:
-                print("\n輸入[代號]選擇日期: ", end="")
-                select = int(msvcrt.getch().decode())
+                print("\n輸入[代號]選擇日期: ", end="", style="bold green")
+                select = self.Redemption_Data.get( # 取得對應 Key 值
+                    int(msvcrt.getch().decode()) # 讀取數字輸入
+                )
 
-                if select > length:
-                    print("\n錯誤的代號, 請重新選擇")
+                if select is None:
+                    print("\n錯誤的代號, 請重新選擇", style="bold red")
                 else:
                     os.system("cls")
-                    for title, winning in self.Redemption_Data[select].items():
-                        # 將數據列表解析為字典 (測試以下寫法 比列導式快一些, 雖然列導式更簡潔)
-                        for index, number in enumerate(winning[:2]): # 前兩個獎勵等級為, 特別獎, 特獎
-                            self.winning[number[-3:]] = {"level": self.Reward_level[index], "number": number}
-                        for number in winning[2:]: # 後面都是 頭獎
-                            self.winning[number[-3:]] = {"level": self.Reward_level[2], "number": number}
 
-                        print(title)
+                    #! 如需要根據選取月份, 顯示兌換日期, 需要解析此處選擇的 month
+                    print(select["month"], style="bold")
+                    data = select["number"]
+
+                    # 將數據列表解析為字典 (測試以下寫法 比列導式快一些, 雖然列導式更簡潔) [Key 值為末三碼]
+                    for index, number in enumerate(data[:2]): # 這個為, 特別獎, 特獎
+                        self.winning[number[-3:]] = {"level": self.Reward_level[index], "number": number}
+                    for number in data[2:]: # 這個都是 頭獎
+                        self.winning[number[-3:]] = {"level": self.Reward_level[2], "number": number}
+
                     break
 
             except ValueError:
-                print("\n代號為數字, 請重新選擇")
+                print("\n代號為數字, 請重新選擇", style="bold red")
 
-        print("\n{:<9} {}".format(self.Reward_level[0], self.Reward_conditions[0]))
+        print("\n{:<9} {}".format(self.Reward_level[0], self.Reward_conditions[0]), style="bold")
         for i in range(1, 8): # 顯示 獎勵等級, 獎勵條件 
-            print("{:<10} {}".format(self.Reward_level[i], self.Reward_conditions[i]))
+            print("{:<10} {}".format(self.Reward_level[i], self.Reward_conditions[i]), style="bold")
+
         self.__Comparison_Date()
 
+    def __call__(self):
+        self.__Select_Date()
+
 if __name__ == "__main__":
-    Comparison().Select_Date()
+    Comparison("https://invoice.etax.nat.gov.tw/index.html")()
